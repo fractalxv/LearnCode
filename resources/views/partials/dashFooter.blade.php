fb2b38dcdf6769a12428eb16b9e51daa5042a100
fractalxv <myraakarimah@gmail.com>
2022-01-13 07:06:38 +0700

    init update

---
+++ resources/views/partials/dashFooter.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.0
+    </div>
+    <strong>©Copyright</strong> 2022 <strong>LearnCode</strong>. Made with ❤️ by<strong>Team 06</strong>
+</footer>
